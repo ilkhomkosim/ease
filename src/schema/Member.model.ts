1bf75e219ff0fb658caf70692b9e0ff41589c3f9
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-05-22 15:51:18 +0900

    feat: develop member schema model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,55 @@
+import mongoose, {Schema} from "mongoose";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
+
+// 1) schemalarmi ikki xil usulda hosil qimaliz birinchisi schema first biz foydalanadigan yani schemalardan foydalanib
+// 2) ikkinchis code first yani codelar orqali schemalarni hosil qilish
+const memberSchema = new Schema({
+    memberType: {
+        type: String,
+        enum: MemberType,
+        default: MemberType.USER
+    },
+
+    memberStatus: {
+        type: String,
+        enum: MemberStatus,
+        default: MemberStatus.ACTIVE,
+    },
+
+    memberNick: {
+        type: String,
+        index: {unique: true, sparse: true},
+        required: true,
+    },
+
+    memberPhone: {
+        type: String,
+        index: {unique: true, sparse: true},
+        required: true,
+    },
+
+    memberPassword: {
+        type: String,
+        select: false,
+        required: true,
+    },
+
+    memberAddress: {
+        type: String,
+    },
+
+    memberDesc: {
+        type: String,
+    },
+
+    memberImage: {
+        type: String,
+    },
+
+    memberPoints: {
+        type: Number,
+        default: 0,
+    },
+}, {timestamps: true});
+
+export default mongoose.model('Member', memberSchema);
