37dd76b27fe1b9b24422dbbeafc0236661da6817
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-05-22 14:36:41 +0900

    fix: modify routing system and controllers

---
+++ src/controllers/shop.controller.ts
@@ -0,0 +1,29 @@
+import {Request, Response} from "express";
+import { T } from "../libs/types/common";
+
+const shopController: T  = {};
+shopController.goHome = (req: Request, res: Response) =>{
+    try{
+        res.send("Home Page");
+    } catch (err) {
+        console.log("Error, goHome:", err);
+    }
+}
+
+shopController.getLogin = (req: Request, res: Response) =>{
+    try{
+        res.send("Login Page");
+    } catch (err) {
+        console.log("Error, getLogin:", err);
+    }
+}
+
+shopController.getSignup = (req: Request, res: Response) =>{
+    try{
+        res.send("Signup Page");
+    } catch (err) {
+        console.log("Error, getSignup:", err);
+    }
+};
+
+export default shopController;
