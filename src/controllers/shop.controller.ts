91e993f412b7cb6fa2ddfbd487325cee93ff4b2f
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-06-03 00:11:44 +0900

    fix: organize SPA and SSR business logic

--- src/controllers/shop.controller.ts
+++ src/controllers/shop.controller.ts
@@ -4,6 +4,8 @@ import MemberService from "../models/Member.service";
 import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
+const memberService = new MemberService();
+
 const shopController: T  = {};
 shopController.goHome = (req: Request, res: Response) =>{
     try{
@@ -11,15 +13,6 @@ shopController.goHome = (req: Request, res: Response) =>{
         res.send("Home Page");
     } catch (err) {
         console.log("Error, goHome:", err);
-    }
-}
-
-shopController.getLogin = (req: Request, res: Response) =>{
-    try{
-        console.log("getLogin");
-        res.send("Login Page");
-    } catch (err) {
-        console.log("Error, getLogin:", err);
     }
 }
 
@@ -32,34 +25,43 @@ shopController.getSignup = (req: Request, res: Response) =>{
     }
 };
 
-shopController.processLogin = async (req: Request, res: Response) =>{
+shopController.getLogin = (req: Request, res: Response) =>{
     try{
-        console.log("processLogin");
-        console.log("body:", req.body);
-        const input: LoginInput = req.body;
-
-        const memberService = new MemberService();
-        const result = await memberService.processLogin(input);
-
-        res.send(result)
+        console.log("getLogin");
+        res.send("Login Page");
     } catch (err) {
-        console.log("Error, processLogin:", err);
-        res.send(err);
+        console.log("Error, getLogin:", err);
     }
-};
+}
 
 shopController.processSignup = async (req: Request, res: Response) =>{
     try{
         console.log("processSignup");
 
         const newMember: MemberInput = req.body;
-        newMember.memberType = MemberType.RESTAURANT;
-
-        const memberService = new MemberService();
+        newMember.memberType = MemberType.SHOP;
         const result = await memberService.processSignup(newMember);
+        //TODO: Session authentication
+        
         res.send(result)
     } catch (err) {
         console.log("Error, processSignup:", err);
+        res.send(err);
+    }
+};
+
+shopController.processLogin = async (req: Request, res: Response) =>{
+    try{
+        console.log("processLogin");
+        console.log("body:", req.body);
+        const input: LoginInput = req.body;
+        const result = await memberService.processLogin(input);
+                //TODO: Session authentication
+
+
+        res.send(result)
+    } catch (err) {
+        console.log("Error, processLogin:", err);
         res.send(err);
     }
 };
