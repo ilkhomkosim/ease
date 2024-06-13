84d31c7eda1a1611e7fa31c1c3cc9717b0e67014
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-06-14 08:17:12 +0900

    feat: configure product Schema model

---
+++ src/libs/enums/product.enum.ts
@@ -0,0 +1,38 @@
+export enum ProductSize {
+    LARGE = "LARGE",
+    NORMAL = "NORMAL",
+    SMALL =  "SMALL",
+    SET = "SET"
+}
+
+export enum ProductStatus {
+    PAUSE = "PAUSE",
+    PROCESS = "PROCESS",
+    DELETE = "DELETE"
+}
+
+export enum ProductVolume {
+    ONE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SET = 12,
+}
+
+export enum ProductCollection{
+    BAD = "BAD",
+    CHAIR = "CHAIR",
+    DESKS = "DESKS",
+    DRESSER = "DRESSER",
+    COUCH = "COUCH",
+    NIGHTSTANDS = "NIGHTSTANDS",
+    OTTOMAN = "OTTOMAN",
+    BOOKCASE = "BOOKCASE",
+    CLOCKS = "CLOCKS",
+    RUSTIC = "RUSTIC",
+    DININGTABLES = "DININGTABLES",
+    TABLES = "TABLES",
+    OTHERS = "OTHERS"
+}
