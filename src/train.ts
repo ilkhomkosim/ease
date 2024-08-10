8b58704f1f3ff9244422519a2532e258b073f66a
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-08-10 20:36:14 +0900

    feat: deploy project via shell and pm2

---
+++ src/train.ts
@@ -0,0 +1,14 @@
+/*
+    PM2 COMMANDS:
+
+    pm2 ls
+    pm2 start dist/server.js --name=EASE
+    pm2 start "npm run start:prod" --name=EASE
+    pm2 stop id
+    pm2 delete id
+    pm2 restart id
+    pm2 monit
+    pm2 kill
+*/
+
+// avval stop qilinadi keyin delete qilinadi pm2 orqali
